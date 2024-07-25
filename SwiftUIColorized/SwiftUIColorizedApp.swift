aa590203003ca628db4d06b969ea7f1bd7d6a2f3
abdul'Karim <iCodeCreator>
2024-07-25 20:43:36 +0300

    Initial Commit

---
+++ SwiftUIColorized/SwiftUIColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorizedApp.swift
+//  SwiftUIColorized
+//
+//  Created by abd ul’Karim 📚 on 25.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
